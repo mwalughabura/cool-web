caa45b272a02638cc0de15ad4bfcffd26df5b37e
Mwalugha Bura <mwalughabura@gmail.com>
2018-07-16 15:10:09 +0300

    Hurray everything in the toggle accordion is working

--- toggle-accordion/main.js
+++ toggle-accordion/main.js
@@ -2,6 +2,8 @@ var accordions = document.getElementsByClassName("accordion");
 
 for (var i = 0; i < accordions.length; i++) {
     accordions[i].onclick = function () {
+        this.classList.toggle("is-open");
+
         var content = this.nextElementSibling;
 
         if (content.style.maxHeight){
