649a8282af7170dabe6251dc135e17fa6d8ed724
Mwalugha Bura <mwalughabura@gmail.com>
2018-07-16 14:59:09 +0300

    All basic functionalities working

---
+++ toggle-accordion/main.js
@@ -0,0 +1,16 @@
+var accordions = document.getElementsByClassName("accordion");
+
+for (var i = 0; i < accordions.length; i++) {
+    accordions[i].onclick = function () {
+        var content = this.nextElementSibling;
+
+        if (content.style.maxHeight){
+            // accordion is open
+            content.style.maxHeight = null;
+        } else {
+            // accordion is closed
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    }
+    
+}
