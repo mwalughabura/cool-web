a76d8ec09ed62a47d2528e1758822f8250ce4348
Mwalugha Bura <mwalughabura@gmail.com>
2018-07-16 13:23:51 +0300

    Save unsaved files

--- accordion-with-js/js/main.js
+++ accordion-with-js/js/main.js
@@ -1,61 +1,26 @@
-//uses classList, setAttribute, and querySelectorAll
-//if you want this to work in IE8/9 youll need to polyfill these
-(function(){
-	var d = document,
-	accordionToggles = d.querySelectorAll('.js-accordionTrigger'),
-	setAria,
-	setAccordionAria,
-	switchAccordion,
-  touchSupported = ('ontouchstart' in window),
-  pointerSupported = ('pointerdown' in window);
-  
-  skipClickDelay = function(e){
-    e.preventDefault();
-    e.target.click();
-  }
+$(function() {
+	var Accordion = function(el, multiple) {
+		this.el = el || {};
+		this.multiple = multiple || false;
 
-		setAriaAttr = function(el, ariaType, newProperty){
-		el.setAttribute(ariaType, newProperty);
-	};
-	setAccordionAria = function(el1, el2, expanded){
-		switch(expanded) {
-      case "true":
-      	setAriaAttr(el1, 'aria-expanded', 'true');
-      	setAriaAttr(el2, 'aria-hidden', 'false');
-      	break;
-      case "false":
-      	setAriaAttr(el1, 'aria-expanded', 'false');
-      	setAriaAttr(el2, 'aria-hidden', 'true');
-      	break;
-      default:
-				break;
-		}
-	};
-//function
-switchAccordion = function(e) {
-  console.log("triggered");
-	e.preventDefault();
-	var thisAnswer = e.target.parentNode.nextElementSibling;
-	var thisQuestion = e.target;
-	if(thisAnswer.classList.contains('is-collapsed')) {
-		setAccordionAria(thisQuestion, thisAnswer, 'true');
-	} else {
-		setAccordionAria(thisQuestion, thisAnswer, 'false');
+		// Variables privadas
+		var links = this.el.find('.link');
+		// Evento
+		links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
 	}
-  	thisQuestion.classList.toggle('is-collapsed');
-  	thisQuestion.classList.toggle('is-expanded');
-		thisAnswer.classList.toggle('is-collapsed');
-		thisAnswer.classList.toggle('is-expanded');
- 	
-  	thisAnswer.classList.toggle('animateIn');
-	};
-	for (var i=0,len=accordionToggles.length; i<len; i++) {
-		if(touchSupported) {
-      accordionToggles[i].addEventListener('touchstart', skipClickDelay, false);
-    }
-    if(pointerSupported){
-      accordionToggles[i].addEventListener('pointerdown', skipClickDelay, false);
-    }
-    accordionToggles[i].addEventListener('click', switchAccordion, false);
-  }
-})();
+
+	Accordion.prototype.dropdown = function(e) {
+		var $el = e.data.el;
+			$this = $(this),
+			$next = $this.next();
+
+		$next.slideToggle();
+		$this.parent().toggleClass('open');
+
+		if (!e.data.multiple) {
+			$el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+		};
+	}	
+
+	var accordion = new Accordion($('#accordion'), false);
+});
