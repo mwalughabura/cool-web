ff37660a544e9b0c58e6eb43a6111ded9c405acc
Mwalugha Bura <mwalughabura@gmail.com>
2018-07-27 09:59:50 +0300

    The first element is visible by default

--- sidetabs/js/main.js
+++ sidetabs/js/main.js
@@ -4,7 +4,7 @@ function openCity(evt, cityName) {
 
     // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
+    for (i = 1; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
