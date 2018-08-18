16e910ca41e96b64015a2b42daa9dea782ef8a7d
Mwalugha Bura <mwalughabura@gmail.com>
2018-08-18 12:18:32 +0300

    [Slide Show JS] Change the file extension from jpg to png

--- slideshowjs/js/main.js
+++ slideshowjs/js/main.js
@@ -1,7 +1,7 @@
 var i = 0;
 
 function change_image (){
-    document.getElementById("slideshow").src = "../img/img"+(i++ % 6)+".jpg";
+    document.getElementById("slideshow").src = "../img/img"+(i++ % 6)+".png";
 }
 
 function slider() {
